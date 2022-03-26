614fae3231bab2ec2e933729aaaaa8446c0cc88a
Mete <37701679+metezd@users.noreply.github.com>
2022-03-26 13:50:21 +0300

    Update Diskriminant.py
    .bat dosyası kaldırıldı (hepsini bir .exe altında toplayacağım için)
    Diskriminant.py'de sonsuz döngü eklendi
    Kaynakça çıkarıldı

--- Diskriminant.py
+++ Diskriminant.py
@@ -1,26 +1,38 @@
-# ikinci dereceden bir denklemin koklerini bulmak icin yapılmıştır
 import math
-print("ikinci dereceden bir denklemin kokunu bulmak icin asagidaki degerleri girin")
-print()
+import os
 
-a = int(input("A icin bir deger girin : "))
-b = int(input("B icin bir deger girin : "))
-c = int(input("C icin bir deger girin : "))
+count = 0
+while count < 5:
+    os.system('title 2 bilinmeyenli bir denklemin kökünü bulma')
+    os.system('color a') # yeşil rengi sağlar
+    os.system('powershell write-host -fore Red Iki bilinmeyenli bir denklemin kokunu bulma [Matematik Projesi]')
 
-diskriminant = (b * b) - (4 * a * c)
-# https://caylakyazilimci.com/post/if-else-ifadeleri-python-101-3
-# math.sqrt - sqaure root demek yani karekök
-if(diskriminant > 0):
-    kok1 = (-b + math.sqrt(diskriminant)) / (2 * a)
-    kok2 = (-b - math.sqrt(diskriminant)) / (2 * a)
     print()
-    print("Iki farkli gercek kok var: kok1 = %.2f and kok2 = %.2f" %(kok1, kok2))
-elif(diskriminant == 0):
-    kok1 = kok2 = -b / (2 * a)
+    print("ikinci dereceden bir denklemin kokunu bulmak icin asagidaki degerleri girin")
     print()
-    print("Iki esit gercek kok var: kok1 = %.2f and kok2 = %.2f" %(kok1, kok2))
-elif(diskriminant < 0):
-    kok1 = kok2 = -b / (2 * a)
-    imaginary = math.sqrt(-diskriminant) / (2 * a)
+
+    a = int(input("A icin bir deger girin : "))
+    b = int(input("B icin bir deger girin : "))
+    c = int(input("C icin bir deger girin : "))
+
+    diskriminant = (b * b) - (4 * a * c)
+
+    if(diskriminant > 0):
+        kok1 = (-b + math.sqrt(diskriminant)) / (2 * a)
+        kok2 = (-b - math.sqrt(diskriminant)) / (2 * a)
+        print()
+        print("2 farkli gercek kok var: kok1 = %.2f and kok2 = %.2f" %(kok1, kok2))
+    elif(diskriminant == 0):
+        kok1 = kok2 = -b / (2 * a)
+        print()
+        print("2 esit gercek kok var: kok1 = %.2f and kok2 = %.2f" %(kok1, kok2))
+    elif(diskriminant < 0):
+        kok1 = kok2 = -b / (2 * a)
+        imaginary = math.sqrt(-diskriminant) / (2 * a)
+        print()
+        print("2 farklı karmasik kok var: kok1 = %.2f+%.2f and kok2 = %.2f-%.2f" %(kok1, imaginary, kok2, imaginary))
+
     print()
-    print("Iki farklı karmasik kok var: kok1 = %.2f+%.2f and kok2 = %.2f-%.2f" %(kok1, imaginary, kok2, imaginary))
+    print("Yeniden yapmak icin rastgele bir tusa basin")
+    os.system ('pause > nul') # input("Bir daha yapmak icin rastgele bir tusa basin...")
+    os.system('cls' if os.name == 'nt' else 'clear')
